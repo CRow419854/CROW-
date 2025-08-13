110703a0ebbc1b88e9570c2cafa35677da66eef6
CRow419854 <CMM95158@GMAIL.COM>
2025-08-14 00:22:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://crow9298:Vb9NZIBmCxIs7fJs@cluster0.pgsbafo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
